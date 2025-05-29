5dbabcce814119cf3f0a2ee5ec7c852947f8fdf4
Никита <153456515+Lairont@users.noreply.github.com>
2025-05-29 10:39:49 +0300

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Первая сторона: ");
+            double side1 = double.Parse(Console.ReadLine());
+            Console.Write("Вторая сторона: ");
+            double side2 = double.Parse(Console.ReadLine());
+            Rectangle rec = new Rectangle(side1, side2);
+            Console.WriteLine($"Площадь: {rec.AreaCalculator()}");
+            Console.WriteLine($"Периметр: {rec.PerimeterCalculator()}");
+            Console.ReadKey();
+             
+        }
+    }
+}
---
+++ ConsoleApp1/Rectangle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Rectangle
+    {
+        double side1;
+        double side2;
+        public Rectangle()
+        {
+
+        }
+        public Rectangle(double side1, double side2)
+        {
+            this.side1 = side1;
+            this.side2 = side2;
+        }
+        public double AreaCalculator()
+        {
+            return side1 * side2;
+        }
+        public double PerimeterCalculator()
+        {
+            return 2 * (side1 + side2);
+        }
+        public double Area { get; }
+        public double Perimeter { get; }
+
+    }
+}
