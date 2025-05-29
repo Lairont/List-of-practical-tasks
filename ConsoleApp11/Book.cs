815bc69bbbed77abdf42dc7aa053cc9f74f19646
Никита <153456515+Lairont@users.noreply.github.com>
2025-05-29 11:16:49 +0300

    Add files via upload

---
+++ ConsoleApp11/Book.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    class Book
+    {
+        private Author author;
+        private Title title;
+        private Content content;
+
+        internal Author Author { get => author; set => author = value; }
+        internal Title Title { get => title; set => title = value; }
+        internal Content Content { get => content; set => content = value; }
+
+        public Book(Author author, Title title, Content content)
+        {
+            this.Author = author;
+            this.Title = title;
+            this.Content = content;
+        }
+        public void Show()
+        {
+            Author.Show();
+            Title.Show();
+            Content.Show();
+        }
+    }
+}
---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Author author = new Author("Пушкин");
+            Title title = new Title("Евгений Онегин");
+            Content content = new Content("Любовь");
+            Book book = new Book(author, title, content);
+            book.Show();
+            Console.Read();
+        }
+    }
+}
