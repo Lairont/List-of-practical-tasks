bed5f193efe90736b3d7019125bc244c35890971
Никита <153456515+Lairont@users.noreply.github.com>
2025-05-30 12:49:23 +0300

    Add files via upload

---
+++ ConsoleApp12/Triangle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    class Triangle <T>
+    {
+        private T a;
+        private T b;
+        private T c;
+
+        public T A { get => a; set => a = value; }
+        public T B { get => b; set => b = value; }
+        public T C { get => c; set => c = value; }
+        public Triangle() { }
+        public Triangle(T a, T b, T c)
+        {
+            this.A = a;
+            this.B = b;
+            this.C = c;
+        }
+        public void Info()
+        {
+            Console.WriteLine($"a= {A}\nb= {B}\nc= {C}");
+        }
+    }
+}
