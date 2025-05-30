bee5eacf307f2596757f5cfcb067eca7b01168b4
Никита <153456515+Lairont@users.noreply.github.com>
2025-05-30 12:42:51 +0300

    Add files via upload

---
+++ ConsoleApp11/Point.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    class Point<T>
+    {
+        public T X { get; set; }
+        public T Y { get; set; }
+        public Point() { }
+        public Point(T x, T y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+        public void Info()
+        {
+            Console.WriteLine($"x= {X}\ny= {Y}"); 
+        }
+    }
+}
---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point<int> pint = new Point<int>(45, 82);
+            Point<double> pdouble = new Point<double>(45.2, 82.1);
+            pint.Info();
+            Console.WriteLine();
+            pdouble.Info();
+            Console.ReadLine();
+        }
+    }
+}
