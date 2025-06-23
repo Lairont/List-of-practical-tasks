df5a73d5d56a8f8128b3182169cb957ad981ac11
Никита <153456515+Lairont@users.noreply.github.com>
2025-06-24 00:44:03 +0300

    Add files via upload

---
+++ 34/DerivedClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _34
+{
+    class DerivedClass : IInterface3
+    {
+        // 6. Реализация всех трёх методов
+        public void Method1()
+        {
+            Console.WriteLine("Метод Method1");
+        }
+
+        public void Method2()
+        {
+            Console.WriteLine("Метод Method2");
+        }
+
+        public void Method3()
+        {
+            Console.WriteLine("Метод Method3");
+        }
+    }
+}
---
+++ 34/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _34
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 7. Создание объекта и вызов всех методов
+            DerivedClass dClass = new DerivedClass();
+
+            dClass.Method1();
+            dClass.Method2();
+            dClass.Method3();
+
+            Console.ReadKey();
+        }
+    }
+}
