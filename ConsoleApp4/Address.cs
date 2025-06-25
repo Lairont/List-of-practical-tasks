1f3c1065c5e4f6a359338d60f220d2dabaf656d8
Никита <153456515+Lairont@users.noreply.github.com>
2025-06-26 02:17:36 +0300

    Add files via upload

---
+++ ConsoleApp4/Address.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Address
+    {
+        private string index;
+        private string country;
+        private string city;
+        private string street;
+        private string house;
+        private string apartment;
+
+       
+        public string Index 
+        {
+            get
+            {
+                // Возвращаем значение поля index
+                return index;
+            }
+            set
+            {
+                // Записываем значение в поле index
+                index = value;
+            }
+        }
+
+        public string Country
+        {
+            get
+            {
+                return country;
+            }
+            set
+            {
+                country = value;
+            }
+        }
+
+        public string City
+        {
+            get
+            {
+                return city;
+            }
+            set
+            {
+                city = value;
+            }
+        }
+
+        public string Street
+        {
+            get
+            {
+                return street;
+            }
+            set
+            {
+                street = value;
+            }
+        }
+
+        public string House
+        {
+            get
+            {
+                return house;
+            }
+            set
+            {
+                house = value;
+            }
+        }
+
+        public string Apartment
+        {
+            get
+            {
+                return apartment;
+            }
+            set
+            {
+                apartment = value;
+            }
+        }
+    }
+}
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создаем экземпляр класса Address
+            Address myAddress = new Address();
+
+            // Заполняем данные через свойства
+            myAddress.Index = "101015";
+            myAddress.Country = "Россия";
+            myAddress.City = "Москва";
+            myAddress.Street = "Тверская";
+            myAddress.House = "15";
+            myAddress.Apartment = "45";
+
+            // Выводим данные
+            Console.WriteLine("Почтовый адрес:");
+            Console.WriteLine("Индекс: " + myAddress.Index);
+            Console.WriteLine("Страна: " + myAddress.Country);
+            Console.WriteLine("Город: " + myAddress.City);
+            Console.WriteLine("Улица: " + myAddress.Street);
+            Console.WriteLine("Дом: " + myAddress.House);
+            Console.WriteLine("Квартира: " + myAddress.Apartment);
+
+            Console.ReadKey();
+        }
+    }
+}
+
