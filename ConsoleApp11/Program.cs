d13dfe2a89e516c74fd63b2d9b9913a7eb4c1573
Никита <153456515+Lairont@users.noreply.github.com>
2025-06-26 02:48:55 +0300

    Add files via upload

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите ключ доступа (pro / exp): ");
+            string key = Console.ReadLine();
+
+            DocumentWorker worker;
+
+            if (key == "pro")
+            {
+                worker = new ProDocumentWorker();
+            }
+            else if (key == "exp")
+            {
+                worker = new ExpertDocumentWorker();
+            }
+            else
+            {
+                worker = new DocumentWorker();
+            }
+
+            // Демонстрация работы методов
+            worker.OpenDocument();
+            worker.EditDocument();
+            worker.SaveDocument();
+
+            Console.ReadKey();
+        }
+    }
+}
+
