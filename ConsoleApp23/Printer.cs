43826ff462392ea473fa55beb65adaf6deb481f1
Никита <153456515+Lairont@users.noreply.github.com>
2025-06-26 02:27:48 +0300

    Add files via upload

---
+++ ConsoleApp23/Printer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp23
+{
+    enum MyColor
+    {
+        Black = 0,
+        Blue = 1,
+        Green = 2,
+        Cyan = 3,
+        Red = 4,
+        Magenta = 5,
+        Yellow = 6,
+        White = 7
+    }
+
+    class Printer
+    {
+        public static void Print(string stroka, int color)
+        {
+            if (Enum.IsDefined(typeof(MyColor), color))
+            {
+                Console.ForegroundColor = (ConsoleColor)color;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            Console.WriteLine(stroka);
+            Console.ResetColor();
+        }
+    }
+}
---
+++ ConsoleApp23/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ввод строки
+            Console.Write("Введите строку: ");
+            string input = Console.ReadLine();
+
+            // Вывод доступных цветов
+            Console.WriteLine("Доступные цвета:");
+            foreach (var colorName in Enum.GetValues(typeof(MyColor)))
+            {
+                Console.WriteLine($"{(int)colorName}. {colorName}");
+            }
+
+            // Ввод номера цвета
+            Console.Write("Введите номер цвета: ");
+            int colorNumber;
+            if (!int.TryParse(Console.ReadLine(), out colorNumber))
+            {
+                Console.WriteLine("Некорректный ввод номера цвета.");
+                return;
+            }
+
+            // Печать строки в выбранном цвете
+            Printer.Print(input, colorNumber);
+
+            Console.ReadKey();
+        }
+    }
+}
