50738131e1dae87ebf597b959c27fe2c484632c2
Никита <153456515+Lairont@users.noreply.github.com>
2025-06-26 02:19:19 +0300

    Add files via upload

---
+++ ConsoleApp3/Figure.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Figure
+    {
+        private Point[] points;
+
+        public Figure(Point[] points)
+        {
+            this.points = points;
+        }
+
+        public double LengthSide(Point A, Point B)
+        {
+            return Math.Sqrt(Math.Pow(B.X - A.X, 2) + Math.Pow(B.Y - A.Y, 2));
+        }
+
+        public void PerimeterCalculator()
+        {
+            double perimeter = 0;
+            for (int i = 0; i < points.Length; i++)
+            {
+                Point current = points[i];
+                Point next = points[(i + 1) % points.Length];  // последняя соединяется с первой
+                perimeter += LengthSide(current, next);
+            }
+
+            Console.WriteLine($"Название фигуры: {points[0].Name}");
+            Console.WriteLine($"Периметр: {perimeter:F2}");
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point[] points = {
+            new Point(0, 0, "Triangle"),
+            new Point(3, 0, "Triangle"),
+            new Point(0, 4, "Triangle")
+        };
+
+            Figure figure = new Figure(points);
+            figure.PerimeterCalculator();
+
+
+            Console.ReadKey();
+        }
+    }
+}
