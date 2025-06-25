e449f267e38b8a2cc3e9c8f897d59d84a6c04109
Никита <153456515+Lairont@users.noreply.github.com>
2025-06-26 02:22:35 +0300

    Add files via upload

---
+++ ConsoleApp20/Book.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp20
+{
+    struct Book
+    {
+        // Поля структуры
+        public string Title;   // Название книги
+        public string Author;  // Автор (Ф.И.О.)
+        public int Year;       // Год издания
+        public int Pages;      // Количество страниц
+
+        // Конструктор структуры
+        public Book(string title, string author, int year, int pages)
+        {
+            Title = title;
+            Author = author;
+            Year = year;
+            Pages = pages;
+        }
+
+        // Метод вывода информации о книге
+        public void PrintInfo()
+        {
+            Console.WriteLine("Название: {0}", Title);
+            Console.WriteLine("Автор: {0}", Author);
+            Console.WriteLine("Год издания: {0}", Year);
+            Console.WriteLine("Количество страниц: {0}", Pages);
+            Console.WriteLine("-----------------------------------");
+        }
+    }
+}
---
+++ ConsoleApp20/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Количество книг
+            int n = 3;
+
+            // Создаём массив из n книг
+            Book[] books = new Book[n];
+
+            // Заполняем массив книг
+            books[0] = new Book("Война и мир", "Толстой Л.Н.", 1869, 1300);
+            books[1] = new Book("Преступление и наказание", "Достоевский Ф.М.", 1866, 672);
+            books[2] = new Book("Мастер и Маргарита", "Булгаков М.А.", 1967, 480);
+
+            // Вывод информации о всех книгах
+            Console.WriteLine("Список книг в библиотеке:\n");
+            foreach (Book book in books)
+            {
+                book.PrintInfo();
+            }
+
+            Console.ReadKey(); 
+        }
+    }
+}
