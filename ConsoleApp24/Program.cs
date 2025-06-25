2b34b51e16001b07dbe8001e0f12548c057e2fbf
Никита <153456515+Lairont@users.noreply.github.com>
2025-06-26 02:30:01 +0300

    Add files via upload

---
+++ ConsoleApp24/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Выводим все доступные должности
+            Console.WriteLine("Доступные должности:");
+            foreach (var post in Enum.GetNames(typeof(Post)))
+            {
+                Console.WriteLine(post);
+            }
+
+            // Ввод должности сотрудника
+            Console.Write("Введите должность сотрудника: ");
+            string inputPost = Console.ReadLine();
+
+            // Проверяем, существует ли введённая должность
+            Post workerPost;
+            if (!Enum.TryParse(inputPost, out workerPost))
+            {
+                Console.WriteLine("Некорректная должность.");
+                return;
+            }
+
+            // Ввод количества отработанных часов
+            Console.Write("Введите количество отработанных часов: ");
+            int hours;
+            if (!int.TryParse(Console.ReadLine(), out hours))
+            {
+                Console.WriteLine("Некорректное количество часов.");
+                return;
+            }
+
+            Accountant accountant = new Accountant();
+            bool bonus = accountant.AskForBonus(workerPost, hours);
+
+            // Вывод результата
+            if (bonus)
+                Console.WriteLine("Сотруднику положена премия.");
+            else
+                Console.WriteLine("Сотруднику не положена премия.");
+        }
+    }
+}
+
