80848c8ea4acdc38b2dbfb2fe0b9361802658bfc
Никита <153456515+Lairont@users.noreply.github.com>
2025-06-26 02:26:25 +0300

    Add files via upload

---
+++ ConsoleApp22/Notebook.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22
+{
+    struct Notebook
+    {
+        public string Model;
+        public string Manufacturer;
+        public decimal Price;
+
+        // Конструктор для инициализации полей
+        public Notebook(string model, string manufacturer, decimal price)
+        {
+            Model = model;
+            Manufacturer = manufacturer;
+            Price = price;
+        }
+
+        // Метод для вывода содержимого полей на экран
+        public void PrintInfo()
+        {
+            Console.WriteLine("Информация о ноутбуке:");
+            Console.WriteLine($"Модель: {Model}");
+            Console.WriteLine($"Производитель: {Manufacturer}");
+            Console.WriteLine($"Цена: {Price} руб.");
+        }
+    }
+}
---
+++ ConsoleApp22/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Пример создания структуры через конструктор
+            Notebook notebook = new Notebook("Aspire 7", "Acer", 58999.99m);
+            notebook.PrintInfo();
+
+            Console.WriteLine();
+
+            // Пример создания структуры с ручным вводом пользователя
+            Console.Write("Введите модель ноутбука: ");
+            string model = Console.ReadLine();
+
+            Console.Write("Введите производителя: ");
+            string manufacturer = Console.ReadLine();
+
+            Console.Write("Введите цену: ");
+            decimal price = decimal.Parse(Console.ReadLine());
+
+            Notebook userNotebook = new Notebook(model, manufacturer, price);
+            userNotebook.PrintInfo();
+
+            Console.ReadKey();
+        }
+    }
+}
