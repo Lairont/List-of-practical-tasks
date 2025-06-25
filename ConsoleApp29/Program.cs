83980d90b802ee9a6533ebe7199b7efe2abfa37f
Никита <153456515+Lairont@users.noreply.github.com>
2025-06-26 02:40:31 +0300

    Add files via upload

---
+++ ConsoleApp29/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Список товаров
+            List<Tovar> order = new List<Tovar>
+            {
+                new Tovar("Карандаш", 5, 3),
+                new Tovar("Ручка", 10, 2),
+                new Tovar("Тетрадь", 15, 5),
+                new Tovar("Стерка", 100, -1) // проверка ограничений
+            };
+
+            Console.WriteLine("Список заказанных товаров:\n");
+
+            int totalOrderCost = 0;
+
+            foreach (Tovar t in order)
+            {
+                t.PrintInfo();
+                totalOrderCost += t.GetTotalCost();
+            }
+
+            Console.WriteLine($"\nОбщая стоимость заказа: {totalOrderCost} руб.");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp29/Tovar.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp29
+{
+    class Tovar
+    {
+        private string name;
+        private int price;
+        private int kvo;
+
+        // Свойство для названия
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        // Свойство для цены с ограничением от 1 до 20
+        public int Price
+        {
+            get { return price; }
+            set
+            {
+                if (value >= 1 && value <= 20)
+                    price = value;
+                else
+                    price = 1; // значение по умолчанию
+            }
+        }
+
+        // Свойство для количества с ограничением от 0 до 10
+        public int Kvo
+        {
+            get { return kvo; }
+            set
+            {
+                if (value >= 0 && value <= 10)
+                    kvo = value;
+                else
+                    kvo = 0; // значение по умолчанию
+            }
+        }
+
+        // Конструктор
+        public Tovar(string name, int price, int kvo)
+        {
+            Name = name;
+            Price = price;
+            Kvo = kvo;
+        }
+
+        // Метод для подсчета стоимости конкретного товара
+        public int GetTotalCost()
+        {
+            return Price * Kvo;
+        }
+
+        // Метод для вывода информации о товаре
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Товар: {Name}, Цена: {Price}, Кол-во: {Kvo}, Сумма: {GetTotalCost()} руб.");
+        }
+    }
+}
