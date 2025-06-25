23f5d2ef0d02fc6089286769713840b2967c6ebf
Никита <153456515+Lairont@users.noreply.github.com>
2025-06-26 02:39:18 +0300

    Add files via upload

---
+++ ConsoleApp28/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создаем объект класса Tour
+            Tour myTour = new Tour();
+
+            Console.WriteLine("Вариант 1 (без параметров):");
+            myTour.TourCalc();
+
+            Console.WriteLine("\nВариант 2 (один параметр - страна):");
+            myTour.TourCalc("Италия");
+
+            Console.WriteLine("\nВариант 3 (два параметра - страна и количество дней):");
+            myTour.TourCalc("Испания", 7);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp28/Tour.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp28
+{
+    class Tour
+    {
+        public void TourCalc()
+        {
+            Console.WriteLine("Тур: Минское море");
+            Console.WriteLine("Стоимость: бесплатно");
+        }
+
+        // Один параметр - страна (1 день, 50 руб)
+        public void TourCalc(string country)
+        {
+            Console.WriteLine($"Тур: {country}");
+            Console.WriteLine("Количество дней: 1");
+            Console.WriteLine("Стоимость: 50 руб.");
+        }
+
+        // Два параметра - страна и количество дней (50 * n)
+        public void TourCalc(string country, int days)
+        {
+            int cost = 50 * days;
+            Console.WriteLine($"Тур: {country}");
+            Console.WriteLine($"Количество дней: {days}");
+            Console.WriteLine($"Стоимость: {cost} руб.");
+        }
+    }
+}
