6ffb297ffda2bda282f768ff75f9d11e57bcaa42
Никита <153456515+Lairont@users.noreply.github.com>
2025-06-26 02:29:06 +0300

    Add files via upload

---
+++ ConsoleApp24/Accountant.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp24
+{
+    // Перечисление должностей с количеством часов
+    enum Post
+    {
+        Manager = 160,
+        Developer = 150,
+        Designer = 140,
+        Tester = 130
+    }
+    class Accountant
+    {
+        public bool AskForBonus(Post worker, int hours)
+        {
+            // Если отработано больше, чем положено, то премия положена
+            return hours > (int)worker;
+        }
+    }
+}
