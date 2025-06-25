e781d52146661e260a4802b407041e22f9d8b513
Никита <153456515+Lairont@users.noreply.github.com>
2025-06-26 02:31:11 +0300

    Add files via upload

---
+++ ConsoleApp25/Client.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp25
+{
+    class Client
+    {
+        public string Name { get; set; }
+        public string InsuranceType { get; set; }
+        public double InsuranceAmount { get; set; }
+
+        // Конструктор для инициализации полей
+        public Client(string name, string insuranceType, double insuranceAmount)
+        {
+            Name = name;
+            InsuranceType = insuranceType;
+            InsuranceAmount = insuranceAmount;
+        }
+
+        // Метод для получения информации о клиенте
+        public void GetInfo()
+        {
+            Console.WriteLine($"Имя клиента: {Name}, Вид страховки: {InsuranceType}, Сумма страховки: {InsuranceAmount} руб.");
+        }
+    }
+
+}
---
+++ ConsoleApp25/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создаём массив клиентов
+            Client[] clients = new Client[]
+            {
+                new Client("Иванов И.И.", "Автостраховка", 3000),
+                new Client("Петров П.П.", "Медицинская страховка", 1500),
+                new Client("Сидоров С.С.", "Автостраховка", 1800),
+                new Client("Кузнецова А.А.", "Автостраховка", 2200),
+                new Client("Смирнов В.В.", "Страховка жилья", 5000)
+            };
+
+            Console.WriteLine("Все клиенты:");
+            foreach (var client in clients)
+            {
+                client.GetInfo();
+            }
+
+            Console.WriteLine("\nКлиенты, застраховавшие автомобиль на сумму более 2000 руб:");
+            foreach (var client in clients)
+            {
+                if (client.InsuranceType == "Автостраховка" && client.InsuranceAmount > 2000)
+                {
+                    client.GetInfo();
+                }
+            }
+
+            int autoInsuranceCount = 0;
+            foreach (var client in clients)
+            {
+                if (client.InsuranceType == "Автостраховка")
+                {
+                    autoInsuranceCount++;
+                }
+            }
+
+            Console.WriteLine($"\nКоличество клиентов с автостраховкой: {autoInsuranceCount}");
+
+          
+            Console.WriteLine("\nНажмите любую клавишу для выхода...");
+            Console.ReadKey();
+        }
+    }
+}
