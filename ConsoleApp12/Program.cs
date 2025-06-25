4d9c6b599e5559215d7bc38b0715185bc509e713
Никита <153456515+Lairont@users.noreply.github.com>
2025-06-26 02:50:14 +0300

    Add files via upload

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создаем массив базового типа Printer
+            Printer[] printers = new Printer[]
+            {
+                new Printer(),
+                new RedPrinter(),
+                new GreenPrinter(),
+                new BluePrinter()
+            };
+
+            // Перебираем и вызываем метод Print через приведение типов
+            foreach (var printer in printers)
+            {
+                // Явное приведение к типу Printer (по условию задания)
+                Printer p = (Printer)printer;
+                p.Print("Доброго времени суток!");
+            }
+
+            Console.WriteLine("\nНажмите любую клавишу для выхода...");
+            Console.ReadKey();
+        }
+    }
+}
+
