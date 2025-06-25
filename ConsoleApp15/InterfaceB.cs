aff3790ce46a9e459408b49bf091d87444cde883
Никита <153456515+Lairont@users.noreply.github.com>
2025-06-26 02:57:22 +0300

    Add files via upload

---
+++ ConsoleApp15/InterfaceB.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    interface InterfaceB
+    {
+        void ShowInterfaceB(); // Метод интерфейса B
+    }
+}
---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***Exploring interface Example-3***");
+            Console.WriteLine("***Concept of multiple inheritance through interface***");
+            Console.WriteLine();
+
+            MyClass myClassOb = new MyClass(); // Создаём объект класса MyClass
+
+            // Вызываем все методы
+            myClassOb.ShowInterfaceA();
+            myClassOb.ShowInterfaceB();
+            myClassOb.ShowInterfaceC();
+
+            Console.ReadKey(); 
+        }
+    }
+}
+
