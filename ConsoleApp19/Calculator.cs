a4eaec3c340744e7b2d7132ee24e6454f0bdeb54
Никита <153456515+Lairont@users.noreply.github.com>
2025-06-26 03:04:41 +0300

    Add files via upload

---
+++ ConsoleApp19/Calculator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19
+{
+    class Calculator
+    {
+        // Метод сложения
+        public static double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        // Метод вычитания
+        public static double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        // Метод умножения
+        public static double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        // Метод деления
+        public static double Divide(double a, double b)
+        {
+            if (b != 0)
+                return a / b;
+            else
+            {
+                Console.WriteLine("Ошибка: деление на ноль!");
+                return double.NaN; // Возвращаем "не число", если деление невозможно
+            }
+        }
+    }
+}
---
+++ ConsoleApp19/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 5;
+
+            Console.WriteLine("Операнды: a = {0}, b = {1}\n", a, b);
+
+            Console.WriteLine("Сложение: {0}", Calculator.Add(a, b));
+            Console.WriteLine("Вычитание: {0}", Calculator.Subtract(a, b));
+            Console.WriteLine("Умножение: {0}", Calculator.Multiply(a, b));
+            Console.WriteLine("Деление: {0}", Calculator.Divide(a, b));
+
+            Console.ReadKey();
+        }
+    }
+}
