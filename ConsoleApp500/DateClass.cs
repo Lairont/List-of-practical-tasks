ad37181ed8af901c4c984b13d6870cac4f6edd41
Никита <153456515+Lairont@users.noreply.github.com>
2025-06-27 00:25:00 +0300

    Add files via upload

---
+++ ConsoleApp500/DateClass.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp500
+{
+    class DateClass
+    {
+        // a. поле DateTime data;
+        private DateTime data;
+
+        // b. конструктор без параметров
+        public DateClass()
+        {
+            data = DateTime.Now; // по умолчанию текущая дата
+        }
+
+        // c. конструктор с тремя параметрами – год, месяц, день
+        public DateClass(int year, int month, int day)
+        {
+            data = new DateTime(year, month, day);
+        }
+
+        // d. Свойство для доступа к полю data
+        public DateTime Data
+        {
+            get { return data; }
+            set { data = value; }
+        }
+
+        // e. Метод вычисления предыдущего дня
+        public DateTime PreviousDay()
+        {
+            return data.AddDays(-1);
+        }
+
+        // f. Метод вычисления следующего дня
+        public DateTime NextDay()
+        {
+            return data.AddDays(1);
+        }
+    }
+}
---
+++ ConsoleApp500/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp500
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите год:");
+            int year = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите месяц:");
+            int month = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите день:");
+            int day = int.Parse(Console.ReadLine());
+
+            // 2. Создаем объект класса с использованием конструктора с параметрами
+            DateClass myDate = new DateClass(year, month, day);
+
+            Console.WriteLine("Введённая дата: {0}", myDate.Data.ToShortDateString());
+
+            Console.WriteLine("Предыдущий день: {0}", myDate.PreviousDay().ToShortDateString());
+            Console.WriteLine("Следующий день: {0}", myDate.NextDay().ToShortDateString());
+
+            Console.ReadKey();
+        }
+    }
+}
