c0b8cb3c52ab002ed88cb4deb81604b464164c28
Никита <153456515+Lairont@users.noreply.github.com>
2025-06-26 03:02:24 +0300

    Add files via upload

---
+++ ConsoleApp18/Figura.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp18
+{
+    class Figura
+    {
+        // Перегруженный метод для квадрата (один параметр - сторона квадрата)
+        public void ShowArea(double a)
+        {
+            double area = a * a;
+            Console.WriteLine("Фигура: квадрат");
+            Console.WriteLine("Площадь: {0}", area);
+        }
+
+        // Перегруженный метод для прямоугольника (два параметра - длина и ширина)
+        public void ShowArea(double a, double b)
+        {
+            double area = a * b;
+            Console.WriteLine("Фигура: прямоугольник");
+            Console.WriteLine("Площадь: {0}", area);
+        }
+
+        // Перегруженный метод для трапеции (три параметра - основания и высота)
+        public void ShowArea(double a, double b, double h)
+        {
+            double area = (a + b) / 2 * h;
+            Console.WriteLine("Фигура: трапеция");
+            Console.WriteLine("Площадь: {0}", area);
+        }
+    }
+}
---
+++ ConsoleApp18/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Figura figura = new Figura();
+
+            Console.WriteLine("Расчёт площади различных фигур:\n");
+
+            // Пример для квадрата
+            figura.ShowArea(5);
+
+            Console.WriteLine();
+
+            // Пример для прямоугольника
+            figura.ShowArea(4, 7);
+
+            Console.WriteLine();
+
+            // Пример для трапеции
+            figura.ShowArea(3, 5, 4);
+
+            Console.ReadKey();
+        }
+    }
+}
